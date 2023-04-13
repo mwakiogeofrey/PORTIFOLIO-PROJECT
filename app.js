789bfc67e5a70f454b2ea910189dba0229c716ae
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-04-14 00:17:52 +0500

    linters error fixed

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const menuIcon = document.querySelector('#menuIcon');
-    const navbar = document.querySelector('.navbar');
-    const logo = document.querySelector('.logo');
-    
-    menuIcon.onclick = () => {
-      menuIcon.classList.toggle('uil-multiply');
-      navbar.classList.toggle('active');
-      logo.classList.toggle('active');
-    };
-  
-window.onscroll = () => {
-      menuIcon.classList.remove('uil-multiply');
-      navbar.classList.remove('active');
-      logo.classList.remove('active');
-    };
-})
-   
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.querySelector('#menuIcon');
+  const navbar = document.querySelector('.navbar');
+  const logo = document.querySelector('.logo');
 
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('uil-multiply');
+    navbar.classList.toggle('active');
+    logo.classList.toggle('active');
+  };
 
-
+  window.onscroll = () => {
+    menuIcon.classList.remove('uil-multiply');
+    navbar.classList.remove('active');
+    logo.classList.remove('active');
+  };
+});
