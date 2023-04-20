34f36a2120e7037382993b4553142aa0fbaa6650
mwakiogeofrey <mwakiogeofrey579@gmail.com>
2023-04-20 16:01:13 +0300

    Added window poup to my project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+
   const menuIcon = document.querySelector('#menuIcon');
+  
   const navbar = document.querySelector('.navbar');
   const logo = document.querySelector('.logo');
 
@@ -14,4 +15,169 @@ document.addEventListener('DOMContentLoaded', () => {
     navbar.classList.remove('active');
     logo.classList.remove('active');
   };
+
+  const works = document.querySelector('.otherWorks');
+ const popupArray = [
+ 
+  {
+    id:1,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  },
+  {
+    id:2,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    button: 'see project',
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  },
+  {
+    id:3,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  },
+  {
+    id:4,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  },
+  {
+    id:5,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  },
+  {
+    id:6,
+    Name: 'Profesional Art Printing Data',
+    tital: 'Multi Post Stories',
+    tital2: 'Keeping track of hundreds  of components website',
+    desc:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: ' A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featureimage: 'images/Snapshoot Portfolio.png',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    demoLink: 'https://rahimialijan.github.io/Ecommerce-Website-Full-Responsive/',
+    sourceLink: 'https://github.com/rahimialijan/Ecommerce-Website-Full-Responsive',
+  }];
+ function generatecard (){
+ works.innerHTML =popupArray .map((x) => {
+  const{id,Name,desc,description,featureimage,technologies,tital,tital2,demoLink,sourceLink}=x
+    return `<div class="work2">
+    <div class="work2Info">
+        <h3 class="work2Title">
+            ${Name}
+        </h3>
+        <p class="work2Description">
+           ${description}
+        </p>
+        <ul class="work2Languages">
+          <li class="work2Button">${technologies[0]}</li>
+            <li class="work2Button">${technologies[1]}</li>
+            <li class="work2Button">${technologies[2]}</li>
+        </ul>
+    </div>
+    <div class="projectLink">
+<button data-modal-target="#modal${id}" class="work2Link">See Project</button>
+    </div>
+ </div>
+
+ <div class="windor" id="windor">
+ <button data-close-button class="close-btn">&times;</button>
+                <h1 class="popup-story">${tital}s</h1>
+                <h2 class="keeper">${tital2}</h2>
+                
+                <ul id="languages">
+                    <li>${technologies[0]}</li>
+                    <li>${technologies[1]}</li>
+                    <li>${technologies[2]}</li>
+                </ul>
+                <img class="popup-image" src="${featureimage}" alt="Snapshoot">
+                <p id="loren">${desc}</p>
+                <div class="popup-btn-wrapper">
+                    <button type="button" class="btn-popup-btn">
+                      See Live
+                      <i
+                        class="fa-solid fa-arrow-up-right-from-square"
+                      ></i></button
+                    ><button type="button" class="btn-popup-btn">
+                      See Source <i class="fa-brands fa-github"></i>
+                    </button>
+                  </div>
+            </div>
+            <div id="overlay"></div>
+            `;
+  }).join('');
+};
+generatecard();
+
+const openModalBtn=document.querySelectorAll('[data-modal-target]');
+const closeModalBtn=document.querySelectorAll('[data-close-button]');
+const overlay=document.getElementById('overlay');
+
+
+function openModal(windor) {
+  if (windor == null) return;
+  windor.classList.add('active');
+  overlay.classList.add('active');
+}
+function closeModal(windor) {
+  if (windor == null) return;
+  windor.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+
+closeModalBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const windor = button.closest('.windor');
+    closeModal(windor);
+  });
 });
+openModalBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const windor = document.querySelector(button.dataset.modalTarget);
+    openModal(windor);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.windor.active');
+  modals.forEach((windor) => {
+    closeModal(windor);
+  });
+});
+
+
